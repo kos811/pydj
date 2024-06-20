85a51175f3ad22debe4869a7053e8bd8c424f604
Ten Konstantin <kten@ozon.ru>
2024-06-20 13:09:34 +0300

    raw sql

--- department/views.py
+++ department/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Department
-from django.db.models import Avg, F, Window, Max, Subquery, OuterRef
+from django.db.models import Subquery, OuterRef
 from employee.models import Employee
+from django.db.models.expressions import RawSQL
 
 def department(request):
   departments = Department.objects.all().values()
@@ -22,14 +23,37 @@ def details(request, id):
   return HttpResponse(template.render(context, request))
 
 def testing(request):
-  departments = Department.objects.all()
   newest=Employee.objects.filter(department=OuterRef('pk')).order_by('-salary')
-  q = Department.objects.annotate(
-    max_salary=Subquery(newest.values('salary')[:1])).order_by('max_salary') 
+  querySet = Department.objects.annotate(
+    max_salary=Subquery(newest.values('salary')[:1])).order_by('-max_salary') 
+  
+  name_map = {"id": "id", "name": "name", "employee_name":"employee_name", "max_salary":"max_salary"}
+  raw = Department.objects.raw("""                               
+                                with cte as (
+                                  select
+                                    id,
+                                    department_id,
+                                    salary,
+                                    name,
+                                    dense_rank() over (partition by department_id order by salary desc)  as rank
+                                  from employee_employee )
+                                select
+                                  d.id,
+                                  d.name,
+                                  cte.name as employee_name,
+                                  cte.salary as max_salary
+                                from department_department d
+                                join cte on cte.department_id = d.id
+                                where rank = 1
+                                order by cte.salary desc
+                               """,translations=name_map)
+
   template = loader.get_template('testing.html')
+  
+
   context = {
-    'department': departments,
-    'q':q
+    'querySet':querySet,
+    'raw':raw
     # 'employees': employees,
   }
   return HttpResponse(template.render(context, request))
